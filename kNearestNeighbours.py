677055ffb1eb17b947dd9a0a3b5ab096ea17d531
Peter Baldry <peter.baldry@grabba.com>
2020-01-21 13:58:02 +1000

    updated skeleton classification

--- kNearestNeighbours.py
+++ kNearestNeighbours.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 
 
 class KNearestNeighbours:
@@ -9,14 +9,57 @@ class KNearestNeighbours:
         self.testData = testData
         self.method = method
 
-    def classify(self):
+    def classify_test_data(self):
         """
-
-
         Returns: the classified datapoints
         -------
         """
+        classifications = []
+        for dataPoint in testData:
+            #we want to classify every data point
 
+            #get the k closest points
+            closestPoints = self.get_closest_k_points(dataPoint)
+            classification = self.classify_point(closestPoints)
+            classifications.append(classification)
+
+        return classifications
+
+
+    def classify_point(self, closestPoints):
+        """
+
+        Parameters
+        ----------
+        closestPoints
+
+        Returns
+        -------
+
+        """
+        label_counts = []
+        for closestPoint in closestPoints:
+            # the last column of the dataPoint is the label
+            label = closestPoint[-1]
+            label_counts.append(label)
+
+        # get the label which occurs the most - thats the classification
+        classification = self.most_frequent(label_counts)
+        return classification
+
+
+    def most_frequent(self, list):
+        """
+
+        Parameters
+        ----------
+        list
+
+        Returns
+        -------
+
+        """
+        return max(set(list), key=list.count)
 
     def get_closest_k_points(self, dataPoint):
         """
@@ -28,6 +71,11 @@ class KNearestNeighbours:
         -------
 
         """
+        distances = []
+        for training_point in self.trainingData:
+            distance = self.get_distance(training_point, dataPoint)
+            distances.append([distance, training_point])
+
 
 
 
@@ -39,16 +87,19 @@ class KNearestNeighbours:
 
         """
 
+        plt.scatter(self.trainingData[:,0], self.trainingData[:,1])
+        plt.scatter(self.testData[:,0], self.testData[:,1])
 
 
-    def get_distance(self, point1, point2, method):
+
+
+    def get_distance(self, point1, point2):
         """
 
         Parameters
         ----------
         point1
         point2
-        method
 
         Returns
         -------
