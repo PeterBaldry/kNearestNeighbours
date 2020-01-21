ca0432787427a37f8538b06f1ba45d5792f35d56
Peter Baldry <peter.baldry@grabba.com>
2020-01-21 13:32:32 +1000

    skeleton kNearestNeighbours complete, changed docstring style

---
+++ kNearestNeighbours.py
@@ -0,0 +1,60 @@
+
+
+
+class KNearestNeighbours:
+
+    def __init__(self, trainingData, k, testData, method = 'euclidean'):
+        self.k = k
+        self.trainingData = trainingData
+        self.testData = testData
+        self.method = method
+
+    def classify(self):
+        """
+
+
+        Returns: the classified datapoints
+        -------
+        """
+
+
+    def get_closest_k_points(self, dataPoint):
+        """
+        Parameters
+        ----------
+        dataPoint: the coordinate of the datapoint to be classified
+
+        Returns
+        -------
+
+        """
+
+
+
+    def plot_data(self):
+        """
+
+        Returns
+        -------
+
+        """
+
+
+
+    def get_distance(self, point1, point2, method):
+        """
+
+        Parameters
+        ----------
+        point1
+        point2
+        method
+
+        Returns
+        -------
+
+        """
+
+
+
+
